555317727ac21491ca1fdc2755ab43b882b1dff9
Seyyid Muhammed Celik <seyyidmcelik@gmail.com>
2023-11-09 20:53:30 +0300

    add env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,12 +10,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdLpcPAPR6z2rwQFHFIe0lJ7N5-zhiytg",
-  authDomain: "photo-generator-78d6d.firebaseapp.com",
-  projectId: "photo-generator-78d6d",
-  storageBucket: "photo-generator-78d6d.appspot.com",
-  messagingSenderId: "941193809397",
-  appId: "1:941193809397:web:464189d1502a6923a6e4bc",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
@@ -25,6 +25,6 @@ const Auth = getAuth(app);
 const Provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 const store = getStorage(app);
-const API_TOKEN = "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";
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
 
 export { Auth, Provider, db, store, API_TOKEN };
